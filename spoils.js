8e1ee164c4af04e1f573380b561858a0ed41aca6
Ufuueueuru <theultimatewiseguy@gmail.com>
2017-08-15 21:44:03 -0700

    Update spoils.js

--- spoils.js
+++ spoils.js
@@ -2,7 +2,7 @@ function setup(){
   createCanvas(windowWidth,windowHeight);
 }
 function draw(){
-  background(0,0,0);
+  background(100,0,0);
   fill(255,255,255);
   ellipse(mouseX,mouseY,50,50);
 }
