368114433a3b78b60999880fcb37d5dfb52aa0d5
Ufuueueuru <theultimatewiseguy@gmail.com>
2017-08-16 17:01:44 -0700

    Update spoils.js

--- spoils.js
+++ spoils.js
@@ -6,7 +6,7 @@ function draw(){
   noStroke();
   fill(255,255,255);
   if(mouseIsPressed){
-    ellipse(mouseX,mouseY,dist(mouseX,mouseY,pmouseX,pmouseY),dist(mouseX,mouseY,pmouseX,pmouseY));
+    ellipse(mouseX,mouseY,constrain(50-dist(mouseX,mouseY,pmouseX,pmouseY,0,100)),constrain(50-dist(mouseX,mouseY,pmouseX,pmouseY,0,100)));
   }
 }
 function windowResized(){
