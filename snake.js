3afc7e225a589d62cbeac9cafe69ad816c2da8a4
Ufuueueuru <theultimatewiseguy@gmail.com>
2017-09-19 16:01:21 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,1 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
+function draw() {
+  background(0, 0, 0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
